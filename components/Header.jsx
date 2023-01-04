333f9c0d22cd816bb18f940974f39ae95eb802e5
Michael Chop <007micle007@gmail.com>
2023-01-04 14:38:25 +0300

    Added a Header component to the project

---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from "next/link";
+
+const categories = [
+    {name: 'React', slug: 'react'},
+    {name: 'Vue', slug: 'vue'},
+    {name: 'Web Development', slug: 'webdev'},
+]
+
+const Header = () => {
+    return (
+        <div className={"container mx-auto px-10 mb-8"}>
+            <div className="border-b w-full inline-block border-blue-400 py-8">
+                <div className="md:float-left block">
+                    <Link href={"/"}>
+                        <span className="cursor-pointer font-bold text-4xl text-white">
+                            NextBlog
+                        </span>
+                    </Link>
+                </div>
+                <div className="hidden md:float-left md:contents">
+                    {categories.map(category =>
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className={"md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer"}>
+                                {category.name}
+                            </span>
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
