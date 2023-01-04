898fcf0c4caec70fd70c1294ebdf655142ddf75c
Michael Chop <007micle007@gmail.com>
2023-01-04 14:11:23 +0300

    Made the basic layout of the project

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import {PostCard, Categories, PostWidget} from '../components'
+
+const posts = [
+    {title: 'React Testing', excerpt: 'Learn react testing'},
+    {title: 'React with Tailwind', excerpt: 'Learn react with tailwind'}
+]
+
+const Home = () => {
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+
+                <div className={"lg:col-span-8 col-span-1"}>
+                    {posts.map(post => <PostCard key={post.title} post={post}/>
+                    )}
+                </div>
+
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget/>
+                        <Categories/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
