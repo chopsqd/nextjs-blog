501013887e6f1642f8eea3e12416eebeaeff5eac
Michael Chop <007micle007@gmail.com>
2023-01-05 13:05:43 +0300

    Added an author info component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Image from "next/image";
 
-const Author = () => {
+const Author = ({author}) => {
     return (
-        <div>
-            Author
+        <div className={"flex justify-center items-center flex-col mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20"}>
+            <div className="-mt-24">
+                <Image
+                    src={author.photo.url}
+                    unoptimized
+                    alt={author.name}
+                    height={100}
+                    width={100}
+                    className={"align-middle rounded-full"}
+                />
+            </div>
+            <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+            <p className={"text-white text-lg"}>{author.bio}</p>
         </div>
     );
 };
